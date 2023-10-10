e3001d07ed1af19e343f9e38b55112d9938b2edb
CharlesC30 <CharlesRClark30@gmail.com>
2023-10-10 18:05:19 +0200

    working but checking every slice for thresh+center

---
+++ find_pin_centers.py
@@ -0,0 +1,98 @@
+import os
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import cc3d
+import cv2
+from skimage import measure
+from ccl_2d_thresh import ccl_threshold
+
+target_path = "/lhome/clarkcs/Cu-pins/low_quality"
+os.chdir(target_path)
+
+# # full ranges
+# slice_ranges = {
+#     2174: range(115, 215),
+#     2175: range(118, 218),
+#     2176: range(142, 242),
+#     2189: range(79, 179),
+#     2320: range(98, 198),
+#     2322: range(91, 191),
+#     2323: range(78, 178),
+#     2324: range(114, 214),
+#     2505: range(83, 183),
+# }
+
+# for testing
+slice_ranges = {
+    2174: range(115, 215, 49),
+    2175: range(118, 218, 49),
+    2176: range(142, 242, 49),
+    2189: range(79, 179, 49),
+    2320: range(98, 198, 49),
+    2322: range(91, 191, 49),
+    2323: range(78, 178, 49),
+    2324: range(114, 214, 49),
+    2505: range(83, 183, 49),
+}
+
+def read_dataid(filename):
+    return int(filename[1:7])
+
+
+for filename in os.listdir():
+    if ".hdf5" in filename:
+        dataid = read_dataid(filename)
+        with h5py.File(filename) as file:
+
+            for i in slice_ranges[dataid]:
+                print(filename, i)
+                image = np.array(file["Volume"][i])
+                cv2.normalize(image, image, 0, 256, cv2.NORM_MINMAX)
+
+                thresh = ccl_threshold(image)
+                binary_image = image >= thresh
+
+                labels = cc3d.connected_components(binary_image)
+                dusted_labels = cc3d.dust(labels, threshold=100)
+
+                plt.imshow(image)
+                regionprops = measure.regionprops(dusted_labels)
+                for rp in regionprops:
+                    c = rp.centroid
+                    rect = patches.Rectangle((c[1]-64, c[0]-64), 128, 128, edgecolor="r", facecolor="none")
+                    ax = plt.gca()
+                    ax.add_patch(rect)
+                
+                plt.show()
+
+# for filename in os.listdir():
+#     if ".hdf5" in filename:
+#         dataid = read_dataid(filename)
+#         with h5py.File(filename) as file:
+
+#             for i in slice_ranges[dataid]:
+#                 print(filename, i)
+#                 image = np.array(file["Volume"][i])
+#                 cv2.normalize(image, image, 0, 256, cv2.NORM_MINMAX)
+
+#                 thresh = ccl_threshold(image)
+#                 binary_image = image >= thresh
+
+#                 labels = cc3d.connected_components(binary_image)
+#                 dusted_labels = cc3d.dust(labels, threshold=100)
+
+#                 plt.imshow(image)
+#                 regionprops = measure.regionprops(dusted_labels)
+#                 for rp in regionprops:
+#                     c = rp.centroid
+#                     rect = patches.Rectangle((c[1]-64, c[0]-64), 128, 128, edgecolor="r", facecolor="none")
+#                     ax = plt.gca()
+#                     ax.add_patch(rect)
+                
+#                 plt.show()
+
+
+
+
