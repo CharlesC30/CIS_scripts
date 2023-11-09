1e54fe1a0e62c033f7caf25b0952291b2621f41f
CharlesC30 <CharlesRClark30@gmail.com>
2023-11-09 18:01:50 +0100

    detailing smear idea

--- smear_edges.py
+++ smear_edges.py
@@ -5,4 +5,7 @@ import cv2
 from imgutils import load_and_normalize
 
 def smear_object_to_boundary(image, contour_coords):
-    
+    # idea iterate over the boundary pixels of the image and look "inwards"
+    # when you find the appropriate pixel along object contour set all the pixels
+    # in between to the value at the contour
+    pass
